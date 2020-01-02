b424091d0d8697a20baf223489ab48d226303336
Guilherme França <guii_real@hotmail.com>
2020-01-02 01:14:53 -0300

    Desestructuring

--- bundle.js
+++ bundle.js
@@ -11,4 +11,11 @@ var user = {
 var name = user.name,
     age = user.age,
     city = user.address.city;
+
+function getState(_ref) {
+  var state = _ref.address.state;
+  return state;
+}
+
 console.log(name, age, city);
+console.log(getState(user));
--- index.js
+++ index.js
@@ -7,4 +7,12 @@ const user = {
   }
 };
 const { name, age, address: { city } } = user;
+
+
+function getState({ address: { state } }) {
+  return state;
+}
+
+
 console.log(name, age, city);
+console.log(getState(user));
