2d981793ae1a09e2ab8102434b1cfd2068a665cb
Guilherme França <guii_real@hotmail.com>
2020-01-02 01:56:08 -0300

    Webpack Dev Server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-alert('opa');
+const myPromise = () => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('OK');
+  }, 1000);
+});
+
+async function execPromise() {
+  const resolve = await myPromise();
+  console.log(resolve);
+}
+
+const execPromise2 = async () => {
+  const resolve = await myPromise();
+  console.log(resolve);
+};
+
+
+execPromise();
+execPromise2();
+
