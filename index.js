0ac990b9502fc469651c91956365511445f56c72
Guilherme França <guii_real@hotmail.com>
2020-01-02 01:25:17 -0300

    Rest Operator

--- index.js
+++ index.js
@@ -20,3 +20,13 @@ function sum(...numbers) {
 }
 
 console.log(sum(1, 3, 4));
+
+// SPREAD
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const newArr = [...arr1, ...arr2];
+console.log(newArr);
+
+const user2 = { ...user, company: 'Blueshift'};
+console.log(user2);
