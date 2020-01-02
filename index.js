ef19428fc5d81e4eba7b2a36d751d7a4021c9ad5
Guilherme França <guii_real@hotmail.com>
2020-01-02 01:41:35 -0300

    Webpack

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ import def, { sum as sumFn, sub } from './functions';
 console.log(sumFn(1, 2));
 console.log(sub(2, 1));
 def();
+
+// import * as functions from './functions';
+// console.log(functions.sum(1, 2));
