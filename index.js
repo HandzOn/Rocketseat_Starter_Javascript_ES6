3f93a28ffcd7d26652491e252a13228a4333bb9d
Guilherme França <guii_real@hotmail.com>
2020-01-02 00:24:14 -0300

    Class and inheritance

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-class People {
-  sayHello() {
-
+class List {
+  constructor() {
+    this.data = [];
   }
 
-  other() {
-
+  add(data) {
+    this.data.push(data);
+    console.log(this.data);
   }
 }
+
+class MyList extends List {
+
+}
+
+class TodoList extends List {
+  constructor() {
+    super();
+    this.todos = [];
+  }
+
+  addTodo() {
+    this.todos.push('Novo todo');
+    console.log(this.todos);
+  }
+}
+
+const todoList = new MyList();
+
+document.getElementById('newTodo').onclick = function () {
+  todoList.add('Novo todo');
+};
